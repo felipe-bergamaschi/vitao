c1e008419dab31c0e0c3507704b640f75cedd485
Felipe Bergamaschi <midisoft.corporation@gmail.com>
2024-01-10 00:23:12 -0300

    feat: add page

--- challenge1-vite/src/App.tsx
+++ challenge1-vite/src/App.tsx
@@ -1,35 +1,138 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container">
+      <div className="header">
+        <div>&nbsp;</div>
+
+        <h2>Mon, 25 May</h2>
+
+        <span>ºC</span>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+
+      <div className="body">
+        <div className="content">
+          <h2>London</h2>
+
+          <h1>15º</h1>
+
+          <h3>Feels like 7º</h3>
+        </div>
+
+        <div className="image">
+          <img src="./assets/3D Ico_12.png" />
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+      <div className="divider"></div>
+
+      <div className="hours">
+        <div className="content">
+          <h2>3 AM</h2>
+
+          <h3>15º</h3>
+        </div>
+
+        <div className="content">
+          <h2>5 AM</h2>
+
+          <h3>14º</h3>
+        </div>
+
+        <div className="content">
+          <h2>7 AM</h2>
+
+          <h3>17º</h3>
+        </div>
+
+        <div className="content">
+          <h2>9 AM</h2>
+
+          <h3>18º</h3>
+        </div>
+
+        <div className="content">
+          <h2>11 AM</h2>
+
+          <h3>20º</h3>
+        </div>
+      </div>
+
+      <div className="divider"></div>
+
+      <div className="week">
+
+        <div className="content">
+          <h3>Monday</h3>
+
+          <div className="image">
+            <img src="./assets/3D Ico_01.png" />
+          </div>
+
+          <div>
+            <h3>15º</h3>
+
+            <span>7º</span>
+          </div>
+        </div>
+
+        <div className="content">
+          <h3>Tuesday</h3>
+
+          <div className="image">
+            <img src="./assets/3D Ico_02.png" />
+          </div>
+
+          <div>
+            <h3>15º</h3>
+
+            <span>7º</span>
+          </div>
+        </div>
+
+        <div className="content">
+          <h3>Wednesday</h3>
+
+          <div className="image">
+            <img src="./assets/3D Ico_03.png" />
+          </div>
+
+          <div>
+            <h3>15º</h3>
+
+            <span>7º</span>
+          </div>
+        </div>
+
+        <div className="content">
+          <h3>Thursday</h3>
+
+          <div className="image">
+            <img src="./assets/3D Ico_04.png" />
+          </div>
+
+          <div>
+            <h3>15º</h3>
+
+            <span>7º</span>
+          </div>
+        </div>
+
+        <div className="content">
+          <h3>Friday</h3>
+
+          <div className="image">
+            <img src="./assets/3D Ico_05.png" />
+          </div>
+
+          <div>
+            <h3>15º</h3>
+
+            <span>7º</span>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
-
-export default App
